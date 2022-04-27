46fa57893a83d16aa99b796fdc27cfdbe9f3c63a
daehoonseong <gibeks@hanmail.net>
2022-04-27 17:07:33 +1000

    JS Lecture : built-in object / ~boolean

---
+++ javascript/1.wrapper.js
@@ -0,0 +1,10 @@
+// Wrapper Object (built-in)
+// 원시값을 관련된 빌트인 "객체"로 변환한다.
+const number = 123;
+
+console.log(number.toString());
+// number 원시타입을 감싸고 있는 Number 객체로 바뀜
+
+const text = "text";
+console.log(text.length);
+text.trim();
---
+++ javascript/3.boolean.js
@@ -0,0 +1,23 @@
+// Boolean
+const isTrue = new Boolean(true);
+console.log(isTrue.valueOf());
+
+/**Falshy
+ * false
+ * 0
+ * -0
+ * null
+ * undefined
+ * NaN
+ * ''
+ */
+
+/** Truthy
+ * true
+ * 1
+ * -1
+ * '0' 문자열 아무거나
+ * 'false' 문자열 아무거나2
+ * [] 배열
+ * {} 객체
+ */
